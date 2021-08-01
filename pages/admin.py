95bb5c503e28ec0a554fb59b504f8023d86de5ac
MarcusDMelv <56372598+MarcusDMelv@users.noreply.github.com>
2021-07-31 17:49:17 -0700

    Adding models and Views

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+# import models
+from .models import Team
 # Register your models here.
+#TODO add model 'Team'
+admin.site.register(Team)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Team(models.Model):
+    # TODO creating a model for Exec team
+    # img/full name/jobTitle/facebookLink/googleLink
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    job_title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
+    facebook_link = models.URLField(max_length=100)
+    twitter_link = models.URLField(max_length=100)
+    google_link = models.URLField(max_length=100)
+    create_date = models.DateTimeField(auto_now_add=True)
+    # todo change ui
+    def __str__(self):
+        return self.first_name
